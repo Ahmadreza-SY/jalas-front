de08db0c81c2fc088f7e2bd3a0fe3600a4238652
sh-soltanpour <shahryar2006@gmail.com>
2019-11-29 18:41:14 +0330

    Remove unused elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Meeting from './components/meeting/Meeting';
 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <Meeting id={"5de101629cde7661f67be93b"}/>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-links"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <Meeting id={"5de101629cde7661f67be93b"}/>
     </div>
   );
-}
+};
 
 export default App;
