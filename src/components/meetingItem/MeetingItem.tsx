fad507089edf656cf37ba09027fd628dd96b4dab
ahmadrezasy <ahmadreza.saboor2012@gmail.com>
2019-12-13 20:24:44 +0330

    implement meeting list page

---
+++ src/components/meetingItem/MeetingItem.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
+
+export default class MeetingItem extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			id: "",
+			title: "",
+			state: ""
+		};
+	}
+
+	render() {
+		return (
+			<Link
+				to={"/meeting/" + this.props.id}
+				style={{ color: "inherit", textDecoration: "inherit" }}>
+				<div className="row">
+					<label className="col-md-12">
+						Title: {this.props.title}
+					</label>
+					<label className="col-md-12">
+						State: {this.props.state}
+					</label>
+				</div>
+			</Link>
+		);
+	}
+}
+
+interface Props {
+	id: string;
+	title: string;
+	state: string;
+}
+
+interface State {}
