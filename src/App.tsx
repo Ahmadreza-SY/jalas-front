7f2477930781216650032c515369be0dee39bcdd
sh-soltanpour <shahryar2006@gmail.com>
2019-12-27 16:34:55 +0330

    Add Nav component and update Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import MeetingComponent from './components/meeting/Meeting';
@@ -6,23 +6,39 @@ import MeetingList from './components/meetingList/MeetingList';
 import NewMeeting from './components/newMeeting/NewMeeting';
 import Login from "./components/login/Login";
 import Report from "./components/admin/Report";
+import Nav from './components/common/Nav';
 
-
-const App: React.FC = () => {
-  return (
-    <div className="App App-header">
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/meeting" component={MeetingList}/>
-          <Route exact path="/meeting/new" component={NewMeeting}/>
-          <Route exact path="/meeting/:meetingId" component={MeetingComponent}/>
-          <Route exact path="/meeting/:meetingId/vote/:email" component={MeetingComponent}/>
-          <Route exact path="/report" component={Report}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+export default class App extends Component<Props, State> {
+  render() {
+    return (
+      <div className="App App-header">
+        <div className="container">
+          <Router>
+            <div className="row">
+              <div className="col-3">
+                <Nav/>
+              </div>
+              <div className="col-9 text-left">
+                <Switch>
+                  <Route exact path="/login" component={Login}/>
+                  <Route exact path="/meeting" component={MeetingList}/>
+                  <Route exact path="/meeting/new" component={NewMeeting}/>
+                  <Route exact path="/meeting/:meetingId" component={MeetingComponent}/>
+                  <Route exact path="/meeting/:meetingId/vote/:email" component={MeetingComponent}/>
+                  <Route exact path="/report" component={Report}/>
+                </Switch>
+              </div>
+            </div>
+          </Router>
+        </div>
+      </div>
+    );
+  }
 };
 
-export default App;
+
+interface Props {
+}
+
+interface State {
+}
---
+++ src/components/common/Nav.tsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import ToastUtils from '../../utils/ToastUtils';
+import Api from '../../api/Api';
+import {User} from '../../api/models/UserModels';
+import Header from './Header';
+
+export default class Nav extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {redirect: false, user: undefined}
+  }
+
+  componentDidMount(): void {
+    Api.profile().then(response => {
+      this.setState({user: response.data})
+    })
+  }
+
+  logout() {
+    localStorage.clear();
+    ToastUtils.success("Successfully logged out");
+    window.location.href = '/login'
+  }
+
+  render() {
+    const {user} = this.state;
+    return (
+      <div className="text-left">
+        <Header user={user}/>
+        <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <Link className="nav-link" to="/meeting"
+                aria-controls="v-pills-home" aria-selected="true">Meeting</Link>
+          <Link className="nav-link" to="/meeting/new"
+                aria-controls="v-pills-profile" aria-selected="false">New Meeting</Link>
+          {user && user.isAdmin && <Link className="nav-link" to="/report"
+                                         aria-controls="v-pills-messages" aria-selected="false">Report</Link>}
+          <button className="nav-link clickable btn btn-link text-left" onClick={() => this.logout()}
+                  aria-controls="v-pills-settings" aria-selected="false">Logout
+          </button>
+        </div>
+      </div>)
+  }
+}
+
+
+interface Props {
+}
+
+interface State {
+
+  user: User | undefined,
+  redirect: boolean
+}
